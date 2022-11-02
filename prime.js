635195ead2200912e601384d0a063afef9aec009
vijay4081 <bobadev35@gmail.com>
2022-11-02 22:38:50 +0530

    i will make change in their

--- prime.js
+++ prime.js
@@ -1,6 +1,21 @@
-let num = 13;
+function checkprime(num){
+
 count=0;
 for (let i=1;i<=num;i++){
 if(num%i==0){
 count++;
+}
+}if(count==2){
+return true ;
+}else{
+return false;
+}
+}
+
+
+let bag  = checkPrime(13)
+if(bag=true){
+console.log("prime")
+}else{
+console.log("Not prime")
 }
